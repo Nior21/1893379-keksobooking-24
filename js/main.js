68d313cd9d796c00b0bde9ec2f29fc0471ec88fa
nior21 <lhall@ya.ru>
2021-11-05 15:24:26 +0300

    Добавлены пробелы в параметрах (где указываются default значения); Убран возврат undefined и `console.warn`. Вместо них использованы `throw new Error('error message')`;

--- js/main.js
+++ js/main.js
@@ -1,45 +1,14 @@
 /**
- * Функция getRandomNumber() возвращает псевдослучайное положительное целое число
- * @param {number} min  мининимальное значение, которое может вернуть функция
- * @param {number} max  максимальное значение, которое может вернуть функция
- * @returns {number}    возвращает число в диапазоне от 'min' до 'max', либо undefined
- */
-const getRandomNumber = (min=0, max=100) => {
-  if (min < 0 || max < 0) {
-    // eslint-disable-next-line no-console
-    console.warn('Don\'t use negative number in input. А random number cannot be counted');
-    return undefined;
-  }
-  if (min >= max) {
-    // eslint-disable-next-line no-console
-    console.warn('The minimum number at the input exceeds the maximum. А random number cannot be counted');
-    return undefined;
-  }
-  return min + Math.floor(Math.random() * (max - min));
-};
-
-getRandomNumber();
-
-
-/**
- * Функция getRandomFloatNumber() возвращает псевдослучайное положительное число с указаным уровнем точности
- * @param {number} min                  мининимальное значение, которое может вернуть функция
+ * Функция getRandomNumber() возвращает псевдослучайное положительное число с указанным уровнем точности
+ * @param {number} min                  минимальное значение, которое может вернуть функция
  * @param {number} max                  максимальное значение, которое может вернуть функция
  * @param {number} numberDecimalPlaces  кол-во знаков после запятой у генерируемых чисел
  * @returns {number}                    возвращает число в диапазоне от 'min' до 'max' с указанным уровнем точности, либо undefined
  */
-const getRandomFloatNumber = function (min=0, max=100, numberDecimalPlaces=0) {
-  if (min < 0 || max < 0) {
-    // eslint-disable-next-line no-console
-    console.warn('Don\'t use negative number in input. А random number cannot be counted');
-    return undefined;
-  }
-  if (min >= max) {
-    // eslint-disable-next-line no-console
-    console.warn('The minimum number at the input exceeds the maximum. А random number cannot be counted');
-    return undefined;
-  }
+const getRandomNumber = function (min = 0, max = 100, numberDecimalPlaces = 0) {
+  if (min > max)  { throw new Error('The minimum number at the input exceeds the maximum.'); }
+  if (min < 0)    { throw new Error('Don\'t use negative number in input.'); }
   return Number((min + (Math.random() * (max - min))).toFixed(numberDecimalPlaces));
 };
 
-getRandomFloatNumber();
+getRandomNumber();
