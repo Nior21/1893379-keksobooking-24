a1b483eddd1eeaf5b3c03f5c0588a446b1bf9989
nior21 <lhall@ya.ru>
2021-11-16 22:52:53 +0300

    Реализован механизм генерации полей: - Offer.features, - Offer.description, - Offer.photos, - Location.lat и Location.lng Сгенерирован случайный Lorem Ipsum текст для title и для description

--- js/main.js
+++ js/main.js
@@ -19,16 +19,16 @@ const getRandomNumber = function (min = 0, max = 100, numberDecimalPlaces = 0) {
 
 /** ИНИЦИАЛИЗИРУЕМ КОНСТАНТНЫЕ ОБЪЕКТЫ */
 const TITLES = [
-  'На 7 подушке от счастья',
-  'Шерстяная долина',
-  'Рыбная вечеринка',
-  'Сливочное наслаждение',
-  'Теплое место',
-  'Уютная коробка',
-  'Большой улов',
-  'Дерзкий и ушастый',
-  '9 жизней',
-  'После стирки'];
+  'Cur rumor cantare"',
+  'Nunquam promissio lura"',
+  'Nunquam amor orexis"',
+  'Cur homo unda"',
+  'Cur lapsus studere"',
+  'Nunquam fallere ventus"',
+  'Nunquam locus valebat"',
+  'Nunquam resuscitabo glos"',
+  'Cur abaculus trabem"',
+  'Cur poeta potus"'];
 const TYPES = [
   'palace',
   'flat',
@@ -50,7 +50,17 @@ const FEATURES = [
   'washer',
   'elevator',
   'conditioner'];
-const DESCRIPTION = [];
+const DESCRIPTION = [
+  'Ubi est superbus nix? Classiss sunt brabeutas de raptus competition. Sunt medicinaes pugna festus, albus animalises. Altus olla foris quaestios gabalium est. Cum luna cantare, omnes boreases demitto ferox, varius lumenes.',
+  'Vigil de bassus tabes, imperium stella! Cum castor persuadere, omnes burguses imitari placidus, magnum parmaes. Ecce, peritus vortex! Decor de alter fermium, vitare epos! Sunt onuses talem peritus, pius exsules.',
+  'Particula de pius tus, demitto idoleum! Cum abactus peregrinatione, omnes scutumes promissio festus, gratis domuses. Cannabiss resistere in neuter hortus! Altus, grandis visuss solite examinare de neuter, alter capio.',
+  'Diatria, ionicis tormento, et visus. Cum deus unda, omnes urbses imperium alter, bi-color fluctuies. Ecce, coordinatae! Cur palus manducare? Messis velox ducunt ad nobilis mortem. Brodium de mirabilis vortex, examinare xiphias.',
+  'Secundus brodium absolute gratias cacula est. Ubi est noster bubo? A falsis, amicitia nobilis homo. Secundus visus etiam vitares resistentia est. Peritus, clemens seculas sensim carpseris de secundus, varius abaculus.',
+  'Bulla festus repressor est. Ubi est bassus medicina? Hilotaes favere in infernum! Cum bubo nocere, omnes finises magicae neuter, festus lacteaes. Cum lamia ortum, omnes calcariaes perdere azureus, superbus bullaes.',
+  'Repressor talis index est. Nunquam imperium mineralis. Elogiums manducare, tanquam peritus olla. Indictio de altus mens, imperium eleates! A falsis, palus brevis rumor. Dexter, rusticus nixuss tandem imperium de brevis, neuter elevatus.',
+  'Palus, detrius, et abaculus. Pol, a bene calceus, victrix! Devatios trabem, tanquam barbatus guttus. Itineris tramitems ortum in revalia! Sunt nixuses tractare regius, audax decores. Regius vox patienter desideriums idoleum est.',
+  'Fortiss mori in amivadum! Est lotus advena, cesaris. Cum mens cadunt, omnes fortises acquirere ferox, mirabilis ventuses. Detriuss assimilant, tanquam brevis amicitia. Pius, germanus gabaliums solite contactus de albus, ferox turpis.',
+  'Lapsus de talis rumor, perdere bulla! Ubi est germanus index? A falsis, aonides domesticus visus. Armarium bassus triticum est. Fiscinas studere, tanquam germanus fuga. Zetas sunt eleatess de fortis axona.'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
@@ -66,27 +76,17 @@ const getRandomArrayElement = (elements) =>
 
 /**
  * Функция createAd() генерирует новое объявление
- * @param id
- * @returns {{Offer: {features: string[], rooms: number, address: string, checkin: string, price: number, guests: number, description: string, title: string, type: string, checkout: string, photos: string[]}, Author: {avatar: *}, getTestData(*): *, Location: {lng: number, lat: number}}}
+ * @param {number} id порядковый номер создаваемого объекта в массиве
+ * @returns {{Offer: {features: string[], rooms: number, address: `${number}, ${number}`, checkin: *, price: number, guests: number, description: *, title: string, type: *, checkout: *, photos: string[]}, Author: {avatar: string}, Location: {lng: number, lat: number}}}
  */
 const createAd = function (id) {
   const lat = getRandomNumber(35.65000, 35.70000, 5);
   const lng = getRandomNumber(139.70000, 139.80000, 5);
 
   return {
-    /**
-     * Объект author — описывает автора.
-     * avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
-     * @type {{avatar: *}}
-     */
     Author: {
       avatar: `img/avatars/user${ id < 10 ? `0${ id }` : id }.png`,
     },
-
-    /**
-     * Объект offer — содержит информацию об объявлении.
-     * @type {{features: string[], rooms: number, address: string, checkin: string, price: number, guests: number, description: string, title: string, type: string, checkout: string, photos: string[]}}
-     */
     Offer: {
       'title': TITLES[id],
       'address': `${lat}, ${lng}`,
@@ -97,20 +97,17 @@ const createAd = function (id) {
       'checkin': getRandomArrayElement(CHECKINS),
       'checkout': getRandomArrayElement(CHECKOUTS),
       'features': FEATURES.filter(() => getRandomNumber(0, 1), 0),
-      'description': '', // строка — описание помещения. Придумайте самостоятельно.
-      'photos': ['', '', ''], // массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
+      'description': DESCRIPTION[id],
+      'photos': PHOTOS.filter(() => getRandomNumber(0, 1), 0),
     },
-
-    /**
-     * Объект location — местоположение в виде географических координат
-     * @type {{lng: number, lat: number}}
-     */
     Location: {
-      lat: 0.00000, // getRandomNumber(35.65000, 35.70000, 5), // число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-      lng: 0.00000, // getRandomNumber(139.70000, 139.80000, 5), // число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
+      lat,
+      lng,
     },
   };
 };
 
 /** ГЕРЕНИРУЕМ И ВЫВОДИМ СОЗДАННЫЕ ОБЪЕКТЫ */
-const ads = Array.from ( {length: 4}, (value, id) => createAd(id + 1));
+/**const ads = */Array.from ( {length: 10}, (value, id) => createAd(id + 1));
+
+//console.log(ads);
